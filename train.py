47ac015df18e4878f87fc9ff73fd930a66e91f19
manasvi trivedi <manasvitrivedi@gmail.com>
2021-02-12 02:52:58 +0530

    update files

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 import argparse
 import os
 import numpy as np
@@ -9,48 +11,22 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from sklearn.metrics import roc_auc_score
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-url="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+url="https://raw.githubusercontent.com/manas-v/Capstone-Project-Azure-Machine-Learning-Engineer/main/WA_Fn-UseC_-HR-Employee-Attrition.csv"
 ds=TabularDatasetFactory.from_delimited_files(path=url)
 
-
 def clean_data(data):
-    # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
-
     # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
-    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
-    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
-    contact = pd.get_dummies(x_df.contact, prefix="contact")
-    x_df.drop("contact", inplace=True, axis=1)
-    x_df = x_df.join(contact)
-    education = pd.get_dummies(x_df.education, prefix="education")
-    x_df.drop("education", inplace=True, axis=1)
-    x_df = x_df.join(education)
-    x_df["month"] = x_df.month.map(months)
-    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    x_df = x_df.drop(['EmployeeCount', 'Over18', 'StandardHours'], axis = 1) 
+    x_df = pd.get_dummies(x_df, drop_first=True)
+    y_df = x_df.pop("Attrition")
     return x_df,y_df
-
 
 x, y = clean_data(ds)
 
-# TODO: Split data into train and test sets.
-
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2, random_state=21)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2, random_state=1)
 
 run = Run.get_context()
 
@@ -58,18 +34,20 @@ def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument('--criterion', type=str, default="gini", help="The function to measure the quality of a split. Supported criteria are “gini” for the Gini impurity and “entropy” for the information gain.")
+    parser.add_argument('--splitter', type=str, default="best", help="The strategy used to choose the split at each node. Supported strategies are “best” to choose the best split and “random” to choose the best random split.")
+    parser.add_argument('--max_depth', type=int, help="The maximum depth of the tree. If None, then nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples.")
 
     args = parser.parse_args()
 
-    run.log("Regularization Strength:", np.float(args.C))
-    run.log("Max iterations:", np.int(args.max_iter))
+    run.log("Attribute selection measure:", np.str(args.criterion))
+    run.log("Split Strategy:", np.str(args.splitter))
+    run.log("Maximum Depth of a Tree:", np.float(args.max_depth))
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    model = DecisionTreeClassifier(criterion=args.criterion, splitter=args.splitter, max_depth=args.max_depth).fit(x_train, y_train)
 
-    accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
+    AUC_weighted = roc_auc_score(y_test, model.predict_proba(x_test)[:, 1], average="weighted")
+    run.log("AUC_weighted", np.float(AUC_weighted))
     
     os.makedirs('./outputs', exist_ok=True)    
     joblib.dump(value=model, filename='./outputs/model.joblib')
